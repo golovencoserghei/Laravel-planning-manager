12a9f4a25df056aae51c45c94bdfb14acbb51909
realtek2 <paladium98@gmail.com>
2022-07-03 17:08:56 +0300

    added StandTemplate model

---
+++ app/Models/StandTemplate.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StandTemplate extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'day',
+        'times_range',
+        'stand_id',
+        'congregation_id',
+    ];
+
+    
+}
